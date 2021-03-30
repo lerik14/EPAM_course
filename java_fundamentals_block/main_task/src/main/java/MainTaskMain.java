61629fc2ed4ccfec18c0b3c5ea16e9998f33f0a4
lerik14 <69652826+lerik14@users.noreply.github.com>
2021-03-29 21:04:09 -0400

    Fundamentals block - main and optional tasks (#4)
    * Add main and optional tasks for Java fundamentals block
    
    * Add code review fixed
    Changed implementation findShortestElement, findLongestElement and elementsLongerThanAverageLength methods - there is no printing to console.
    
    * Changed averageLength
    
    * Changed averageLength type

---
+++ java_fundamentals_block/main_task/src/main/java/MainTaskMain.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class MainTaskMain {
+
+    public static void main(String[] args) throws IOException {
+        //Java.Fundamentals Main Task
+        Actions.helloUser();
+        Actions.arrayReverse();
+        Actions.randomNumbers();
+        Actions.sumAndMultiple();
+        Actions.monthNumber();
+    }
+}
---
+++ java_fundamentals_block/optional_task/src/main/java/OptionalTask1.java
@@ -0,0 +1,85 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+public class OptionalTask1 {
+
+    private static BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static int[] getElementsArrayFromConsole() throws IOException {
+        System.out.println("Enter the number of elements:");
+        int numberOfElements = Integer.parseInt(reader.readLine());
+        int[] array = new int[numberOfElements];
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < numberOfElements; i++) {
+            array[i] = Integer.parseInt(reader.readLine());
+        }
+        return array;
+    }
+
+    public static int findShortestElement(int[] array) {
+        int shortestElement = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] < shortestElement) {
+                shortestElement = array[i];
+            }
+        }
+        return  shortestElement;
+    }
+
+    public static int findLongestElement(int[] array) {
+        int longestElement = array[0];
+        for (int i = 1; i < array.length; i++) {
+            if (array[i] > longestElement) {
+                longestElement = array[i];
+            }
+        }
+        return  longestElement;
+    }
+
+    public static void sortByLength(int[] array) {
+        System.out.println("Elements sorted by length");
+        for (int i = 0; i < array.length; i++) {
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[i]) {
+                    int temp = array[i];
+                    array[i] = array[j];
+                    array[j] = temp;
+                }
+            }
+        }
+        for (int element : array) {
+            System.out.print(element +" ");
+        }
+        System.out.println();
+    }
+
+    public static int elemLength(int element) {
+        int elemLength = 0;
+        while (element > 0) {
+            element /= 10;
+            elemLength++;
+        }
+        return elemLength;
+    }
+
+    public static List<Integer> elementsLongerThanAverageLength(int[] array, double averageLength) {
+        List<Integer> elementsLongerThanAverageLength = new ArrayList<>();
+        for (int i = 0; i < array.length; i++) {
+            if (elemLength(array[i]) > averageLength) {
+               elementsLongerThanAverageLength.add(array[i]);
+            }
+        }
+        return elementsLongerThanAverageLength;
+    }
+
+    public static double averageLength(int[] array) {
+        double commonLength = 0;
+        for (int i = 0; i < array.length; i++) {
+            commonLength += elemLength(array[i]);
+        }
+        return commonLength / array.length;
+    }
+}
